a275ddd6ba948c26ef415ae76b75c11641c5a02d
yonghwanjang0 <msbracing@gmail.com>
2021-09-19 00:09:19 +0900

    add function - check video exists function

--- InputValueAnalysis.py
+++ InputValueAnalysis.py
@@ -5,8 +5,6 @@ class InputValueAnalysis:
     def __init__(self):
         self._input_value = None
         self.input_value_type = None
-
-        self.playlist_path = None
         self.vod_id = None
 
         self.playlist_getter = GetPlaylist()
@@ -40,30 +38,43 @@ class InputValueAnalysis:
         else:
             self.input_value_type = "Do_not_Valid_String"
 
-    def get_video_info(self):
-        self.playlist_getter.set_video_id(self.vod_id)
+    def check_exists(self):
+        self.playlist_getter.check_remove_video()
+
+    def get_id_info(self):
         self.playlist_getter.get_token()
         self.playlist_getter.check_subscribers_only()
 
     def get_playlist_path(self):
         self.playlist_getter.get_playlist()
 
-    def run(self, input_value):
+    def run(self, input_value:str):
+        self.playlist_getter.__init__()
         self.input_value = input_value
         self.set_value_type()
         if self.input_value_type != "Do_not_Valid_String":
             self.get_video_id()
         else:
-            return "input value is not valid."
+            return ["not valid"]
 
         if self.vod_id is not None:
-            self.get_video_info()
+            self.playlist_getter.set_video_id(self.vod_id)
+            self.playlist_getter.get_data_json()
+            self.check_exists()
         else:
-            return "input value is not valid."
+            return ["not valid"]
+
+        if not self.playlist_getter.delete_vod:
+            self.get_id_info()
+        else:
+            return ["not exists"]
 
         if not self.playlist_getter.subscribers_only:
             self.get_playlist_path()
             infomation_list = []
+            if self.playlist_getter.playlist_path:
+                infomation_list.append(self.vod_id)
+                infomation_list.append(self.playlist_getter.playlist_path)
             return infomation_list
         else:
-            return "this vod is subscribers only."
+            return ["subscribers only"]
