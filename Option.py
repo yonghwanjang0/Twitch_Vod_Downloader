b7bb518b8716801781339b566c191393e8c1d0a5
yonghwanjang0 <msbracing@gmail.com>
2021-09-21 15:26:20 +0900

    Modified Option dialog

--- Option.py
+++ Option.py
@@ -8,8 +8,15 @@ class OptionDialog(QDialog):
         self.temp_path = option_data[0]
         self.target_path = option_data[1]
 
-        self.temp_path_label = QLabel("Temporary Files Save Path")
         self.target_path_label = QLabel("Save Path")
+        self.temp_path_label = QLabel("Temporary Files Save Path")
+
+        self.target_path_box = QLineEdit(self)
+        self.target_path_box.setReadOnly(True)
+        self.target_path_box.setText(self.target_path)
+        self.target_path_box.setMinimumWidth(500)
+
+        self.target_path_button = QPushButton("change", self)
 
         self.temp_path_box = QLineEdit(self)
         self.temp_path_box.setReadOnly(True)
@@ -18,25 +25,18 @@ class OptionDialog(QDialog):
 
         self.temp_path_button = QPushButton("change", self)
 
-        self.target_path_box = QLineEdit(self)
-        self.target_path_box.setReadOnly(True)
-        self.target_path_box.setText(self.target_path)
-        self.target_path_box.setMinimumWidth(500)
-
-        self.target_path_button = QPushButton("change", self)
-        
         button_box = QDialogButtonBox(
             QDialogButtonBox.Ok | QDialogButtonBox.Cancel, self)
 
         layout = QFormLayout(self)
-        layout.addRow(self.temp_path_label)
-        layout.addRow(self.temp_path_box, self.temp_path_button)
         layout.addRow(self.target_path_label)
         layout.addRow(self.target_path_box, self.target_path_button)
+        layout.addRow(self.temp_path_label)
+        layout.addRow(self.temp_path_box, self.temp_path_button)
         layout.addRow(button_box)
 
-        self.temp_path_button.clicked.connect(self.change_temp_path)
         self.target_path_button.clicked.connect(self.change_target_path)
+        self.temp_path_button.clicked.connect(self.change_temp_path)
 
         button_box.accepted.connect(self.accept)
         button_box.rejected.connect(self.reject)
@@ -50,7 +50,8 @@ class OptionDialog(QDialog):
     def change_path(self, option):
         path = QFileDialog.getExistingDirectory(self)
         if path:
-            path = path + "/"
+            path = path.replace("/", "\\")
+            path = path + "\\"
             if option == "Temp":
                 self.temp_path_box.setText(path)
                 self.temp_path = path
